302ffe4563ac0c75e5ef7a941f9e9772c591f1da
nomanajaz65 <nomanajaz65@gmail.com>
2023-03-11 01:16:06 -0800

    done

---
+++ app.js
@@ -0,0 +1,75 @@
+// var arr = ['saad','noman','nomi']
+// document.write(arr[1])
+
+//  var arr = ['saad','noman','nomi']
+// for (var i =0; i<arr.length;i++){
+//     document.write(arr[i] + " ")
+// }
+
+// var arr = ['saad','noman','nomi']
+// var arr2 = ['munashir']
+
+// for (var i= 0 ; i<arr.length;i++){
+//     document.write(arr[i] + " "+arr2 + " <br>")
+// }
+
+
+// var arr = ['saad','noman','nomi']
+// var arr2 = ['munashir','hehhe']
+
+// for (var i =0; i<arr.length; i++){
+// for (var j=0; j<arr.length; j++){
+//     document.write(arr[i] + " " + arr2[j] + " <br>")
+// }
+
+// // }
+
+// for (var i=1; i<=100; i=i+ 10){
+//     document.write(i, " <br>")
+// }
+
+// for (var i = 1; i <= 100; i = i+10){
+//     for (var j =i; j<i+10; j++){
+
+    
+
+// document.write(j," ")
+// }
+// document.write("<br>")
+// }
+
+// for (var i = 1; i <= 100; i = i + 10 ){
+//     for (var j=i ; j<i+10; j++){
+//         document.write(j," ")
+//     }
+//     document.write("<br>")
+// }
+
+// for (var i=1; i<=5; i++){
+//     for (var j=1; j<=i; j++){
+//         document.write(j,' ')
+
+//     }
+//     document.write('<br>')
+// }
+
+// for (var i =1; i<=5 ;i++){
+// for (var j= 1; j<=i;j++){
+//     document.write(j,' ')
+// }
+// document.write ('<br>')
+// }
+
+// for (var i=1; i<=5;i++){
+//   for (var j=1; j<=i;j++){
+//     document.write ("*",' ')
+//   }
+//   document.write ('<br>')
+// }
+
+for (var i=1; i<=5 ; i++){
+    for (var j=1 ; j<=i;j++){
+        document.write(i,' ')
+    }
+    document.write('<br>')
+}
